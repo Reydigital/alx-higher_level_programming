7fedcd61aa45457232047ac53d4c354ebb20eeb8
Reydigital <regysmarie@gmail.com>
2022-10-26 22:15:37 +0000

    commit

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+
+
+$.getJSON(url, function (data) {
+
+  $('DIV#character').text(data.name);
+
+});
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,13 @@
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+
+
+
+$(this).ready(function () {
+
+  $.getJSON(url, function (data) {
+
+    $('DIV#hello').text(data.hello);
+
+  });
+
+});
